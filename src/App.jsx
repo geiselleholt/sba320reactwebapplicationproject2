2c25388d63b619c774c98ae414ec682dbec831e4
Geiselle Holt <geiselleholt@hotmail.com>
2025-05-23 01:08:35 -0600

    Fixed errors with imports, added Rules page, deleted navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import GamePage from "./pages/GamePage";
+import RulesPage from "./pages/RulesPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
-
-  return <>
-        <NavBar />
+  return (
+    <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/Game" element={<ParkPage />} />
-        <Route path="/Rules" element={<ContactPage />} />
+        <Route path="/Game" element={<GamePage />} />
+        <Route path="/Rules" element={<RulesPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-  </>;
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/RulesPage.jsx
@@ -0,0 +1,3 @@
+export default function RulesPage() {
+  return <h1>RulesPage</h1>;
+}
