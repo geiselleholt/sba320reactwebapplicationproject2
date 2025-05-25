674ab68f5f76af361c4cda31212432ce1ad12aa4
Geiselle Holt <geiselleholt@hotmail.com>
2025-05-25 13:18:35 -0600

    css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ function HomePage() {
     <div className="pageContainer">
       <h1 className="homeTitle">Risk & Reveal</h1>
       <img src={cards} alt="cards spread in a circle" />
-      <h1 className="homeParagraph"> 🍀Test your luck and intuition...</h1>
+      <h1 className="homeParagraph"> 🍀Test your luck and intuition!</h1>
       <h2>Bet on the next card drawn and watch your money grow 🎋</h2>
       <h2> Or watch it fly away 💸</h2>
       <button onClick={() => navigate("/game")} className="startButtonHome">
